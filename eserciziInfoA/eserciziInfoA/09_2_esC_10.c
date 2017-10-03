49fcc829626bd767930d071371849714e8fd59a6
AnnaBernasconi <bernasconi.ann@gmail.com>
2017-10-03 15:25:41 +0200

    esercitazione 02ott

--- eserciziInfoA/eserciziInfoA/09_2_esC_10.c
+++
@@ -1,9 +0,0 @@
-//
-//  09_2_esC_10.c
-//  eserciziInfoA
-//
-//  Created by Anna Bernasconi on 02/10/2017.
-//  Copyright © 2017 abernasconi. All rights reserved.
-//
-
-#include <stdio.h>
--- eserciziInfoA/eserciziInfoA/09_2_esC_11.c
+++
@@ -1,9 +0,0 @@
-//
-//  09_2_esC_11.c
-//  eserciziInfoA
-//
-//  Created by Anna Bernasconi on 02/10/2017.
-//  Copyright © 2017 abernasconi. All rights reserved.
-//
-
-#include <stdio.h>
